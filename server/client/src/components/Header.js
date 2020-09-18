b9de43a5df8310ad3b4cf310d7c2b96fad31cfb9
Giang-Le <giangle0411@gmail.com>
2020-09-18 23:59:05 +1000

    fetchUser action, wire up with Header

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Header extends Component {
+class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return 'Loading'
+      case false:
+        return 'Im logged out'
+      default:
+        return 'Im logged in!'
+    }
+  }
+
   render() {
+    console.log(this.props.auth)
     return (
       <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="left brand-logo">
+        <div className="nav-wrapper">
+          <a href="/" className="left brand-logo">
             Email Feedback App
           </a>
-          <ul class="right ">
-            <li>
-              <a href="#">Login with Google</a>
-            </li>
+          <ul className="right ">
+            {/* <li>
+              <a href="/">Login with Google</a>
+            </li> */}
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     )
   }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth }
+}
+
+export default connect(mapStateToProps)(Header)
