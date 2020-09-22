e879a6c5b9475dae1591c3efcf07bac422e5e472
Giang-Le <giangle0411@gmail.com>
2020-09-22 20:47:08 +1000

    Add credits, heroku prod

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,14 +1,19 @@
 const keys = require('../config/credentials')
 const stripe = require('stripe')(keys.stripeSecretKey)
+const requireLogin = require('../middlewares/requireLogin')
 
 module.exports = (app) => {
-  app.post('/api/stripe', async (req, res) => {
+  // requireLogin is called whenever there's a request come in
+  app.post('/api/stripe', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 500,
       currency: 'aud',
       description: 'A$5.00 for 5 credits',
       source: req.body.id,
     })
-    console.log(charge)
+    req.user.credits += 5
+    const user = await req.user.save()
+
+    res.send(user)
   })
 }
